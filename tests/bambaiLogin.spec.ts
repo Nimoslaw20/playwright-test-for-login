a8fd12754f66deb897f597fb8c28b283cf22a57f
EmmanuelNimo <emmanuel.nimo@getampla.com>
2024-06-08 22:04:37 +0000

    login for bambai - test assessment

---
+++ tests/bambaiLogin.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('Bambai Login Feature', async ({ page }) => {
+  await page.goto('https://bamb.ai/');
+  await expect(page).toHaveTitle(/Bambai/);
+  await page.frameLocator('iframe[title="Landing Page"]').getByRole('link', { name: ' Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Sign-In' })).toBeVisible();
+  await page.getByPlaceholder('Enter your email address or').fill('testaccount@gmail.com');
+  await page.getByPlaceholder('Enter your passcode').fill('password123');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  if (await page.getByText("Unable to login with credentials").isVisible()) {
+    console.log("Invalid access with login credentials");
+  } else {
+    console.log("Valid credentials accepted");
+  }
+  await page.waitForTimeout(2000);
+  await expect(page.getByText('Dashboard')).toBeVisible();
+});
+
+
+
+
+
+
